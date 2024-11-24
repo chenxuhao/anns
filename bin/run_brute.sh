8da95bbb22195bd48f8c21d923d16e20d3a63739
ThinhAlex <86966428+ThinhAlex@users.noreply.github.com>
2024-11-24 12:20:02 -0500

    Update run_brute.sh

--- bin/run_brute.sh
+++ bin/run_brute.sh
@@ -8,8 +8,8 @@ DIM=128
 BIN=brute_force_cpu
 BIN=brute_force_gpu
 
-BIN_PATH=/mnt/d/Research/Code/anns-main/bin
-DATA_PATH=/mnt/d/Research/Code/anns-main/data
+BIN_PATH=~/proj/anns/bin
+DATA_PATH=~/proj/anns/data
 OUTFILE=output.txt
 
 echo "$BIN_PATH/$BIN $DATA_PATH/$DATASET/$DATASET\_base.fvecs \
@@ -21,4 +21,3 @@ $BIN_PATH/$BIN $DATA_PATH/$DATASET/$DATASET\_base.fvecs \
                $DATA_PATH/$DATASET/$DATASET\_query.fvecs \
                $DATA_PATH/$DATASET/$DATASET\_groundtruth.ivecs \
                $NP $DIM $NQ $OUTFILE
-
