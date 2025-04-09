43c6e2132923e77479bcce31dfc2cfed9f5f2bbb
ThinhAlex <86966428+ThinhAlex@users.noreply.github.com>
2025-04-09 13:47:56 -0400

    Add files via upload

---
+++ include/pyanns_utils.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <queue>
+#include <cmath>
+#include <utility>
+
+inline float limit_range(float x) {
+    if (x < 0) {
+        x = 0;
+    } else if (x > 1) {
+        x = 1;
+    }
+    return x;
+}
+
+inline float limit_range_sym(float x) {
+    if (x < -1) {
+        x = -1;
+    } else if (x > 1) {
+        x = 1;
+    }
+    return x;
+}
+
+std::pair<float, float> find_minmax(const float* data, int num_items, float ratio = 0.0f) {
+    int top = static_cast<int>(num_items * ratio) + 1;
+
+    std::priority_queue<float, std::vector<float>, std::greater<float>> max_heap;
+    std::priority_queue<float, std::vector<float>, std::greater<float>> min_heap;
+
+    for (int i = 0; i < num_items; ++i) {
+        float val = data[i];
+
+        if (max_heap.size() < top) {
+            max_heap.push(val);
+        } else if (val > max_heap.top()) {
+            max_heap.pop();
+            max_heap.push(val);
+        }
+
+        if (min_heap.size() < top) {
+            min_heap.push(-val);
+        } else if (val < -min_heap.top()) {
+            min_heap.pop();
+            min_heap.push(-val);
+        }
+    }
+
+    return std::make_pair(max_heap.top(), -min_heap.top());
+}
+
+float find_absmax(const float* data, int num_items, float ratio = 0.0f) {
+    int top = static_cast<int>(num_items * ratio) + 1;
+
+    std::priority_queue<float, std::vector<float>, std::greater<float>> heap;
+
+    for (int i = 0; i < num_items; ++i) {
+        float x = std::abs(data[i]);
+        if (heap.size() < top) {
+            heap.push(x);
+        } else if (x > heap.top()) {
+            heap.pop();
+            heap.push(x);
+        }
+    }
+
+    return heap.top();
+}
+
+float find_absmax_without_drop(const float* data, int num_items) {
+    float result = 0.0f;
+    for (int i = 0; i < num_items; ++i) {
+        result = std::max(result, std::abs(data[i]));
+    }
+    return result;
+}
--- include/utils.hpp
+++ include/utils.hpp
@@ -1,76 +1,138 @@
 #pragma once
 
-#include <queue>
 #include <cmath>
-#include <utility>
+#include <chrono>
+#include <vector>
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <unordered_set>
+#include <random>
 
-inline float limit_range(float x) {
-    if (x < 0) {
-        x = 0;
-    } else if (x > 1) {
-        x = 1;
+#include <omp.h>
+#include <cxxabi.h>
+#include <unistd.h>
+#include <immintrin.h>
+#include <sys/resource.h>
+
+#include "timer.hpp"
+#include "distance.hpp"
+
+struct QueryParams{
+  long k;
+  long beamSize; 
+  double cut;
+  long limit;
+  long degree_limit;
+  QueryParams(long k, long Q, double cut, long limit, long dg) : k(k), beamSize(Q), cut(cut), limit(limit), degree_limit(dg) {}
+  QueryParams() {}
+};
+
+template <typename T>
+class vector_dataset {
+  public:
+    size_t num;
+    int dim;
+    T *dptr;
+    bool INNER_PRODUCT = false;
+
+    vector_dataset() { num = dim = 0; }
+    vector_dataset(const char *filename, size_t m = 0) {
+      num = m; dim = 0;
+      load_vectors(filename);
     }
-    return x;
-}
-
-inline float limit_range_sym(float x) {
-    if (x < -1) {
-        x = -1;
-    } else if (x > 1) {
-        x = 1;
+    vector_dataset(size_t nq, int k) {
+      num = nq;
+      dim = k;
+      dptr = new T[nq*k];
     }
-    return x;
-}
 
-std::pair<float, float> find_minmax(const float* data, int num_items, float ratio = 0.0f) {
-    int top = static_cast<int>(num_items * ratio) + 1;
+    T *operator[](size_t i) { return &dptr[i*dim]; }
+    T* data() { return dptr; }
 
-    std::priority_queue<float, std::vector<float>, std::greater<float>> max_heap;
-    std::priority_queue<float, std::vector<float>, std::greater<float>> min_heap;
+    // read fbin, ibin, u8bin
+    void load_vectors(const char *filename) {
+      //std::cout << filename << " ";
+      std::ifstream in(filename, std::ios::binary);
+      if (!in.is_open()) {
+        fprintf(stderr, "Error: cannot open file %s\n", filename);
+        exit(EXIT_FAILURE);
+      }
 
-    for (int i = 0; i < num_items; ++i) {
-        float val = data[i];
+      in.read((char*)&num, 4);
+      in.read((char*)&dim, 4);
 
-        if (max_heap.size() < top) {
-            max_heap.push(val);
-        } else if (val > max_heap.top()) {
-            max_heap.pop();
-            max_heap.push(val);
+      std::cout << " n = " << num << " dim = " << dim << " \t";
+
+      if ((dim*sizeof(T)) % 32 == 0) {
+      //if (0) {
+        std::cout << " aligned\n";
+        dptr = (T*)aligned_alloc(32, num*dim*sizeof(T));
+      } else {
+        std::cout << " unaligned\n";
+        dptr = (T*)malloc(num*dim*sizeof(T));
+      }
+
+      in.seekg(8, std::ios::beg);
+      for (size_t i = 0; i < num; i++) {
+        in.read((char*)(dptr + i * dim), dim * sizeof(T));
+      }
+      //for (size_t i = 0; i < num; i++) in.read((char*)(dptr+i*dim), sizeof(T)*dim);
+      in.close();
+    }
+};
+
+template <typename T>
+class ANNS {
+private:
+  int K;
+  int nqueries;
+  int vecdim;
+  size_t npoints;
+public:
+  ANNS(int k, int qsize, int dim, size_t dsize,
+       const char* query_file, const char* data_file, 
+       const char* gt_file, const char* out_file, const char *index) :
+      K(k), nqueries(qsize), vecdim(dim), npoints(dsize) {
+    int degree = 32;
+    QueryParams QP(K, 5*K, 0, npoints, degree);
+    vector_dataset<T> data_vectors(data_file);
+    vector_dataset<T> queries(query_file);
+    vector_dataset<int> gt_all(gt_file);
+    vector_dataset<int> results(nqueries, k);
+    Timer t;
+    t.Start();
+    search(K, nqueries, vecdim, npoints, queries.data(), data_vectors.data(), results.data(), index);
+    t.Stop();
+    auto runtime = t.Seconds();
+    auto recall = compute_avg_recall_1D(results, gt_all);
+    printf("total runtime: %f sec, recall: %f\n", runtime, recall);
+  }
+
+  void search(int k, int qsize, int dim, size_t npoints,
+              const T* queries, const T* data_vectors,
+              int *results, const char *index);
+
+  inline double compute_avg_recall_1D(vector_dataset<int> &results, vector_dataset<int> &gt) {
+    assert(results.num == gt.num);
+    assert(results.dim <= gt.dim);
+    size_t qsize = results.num;
+    int K = results.dim;
+    int64_t correct = 0;
+    #pragma omp parallel for reduction(+:correct)
+    for (size_t q_i = 0; q_i < qsize; ++q_i) {
+      for (int top_i = 0; top_i < K; ++top_i) {
+        auto true_id = gt[q_i][top_i];
+        for (int n_i = 0; n_i < K; ++n_i) {
+          if (results[q_i][n_i] == true_id) {
+            correct++;
+            break;
+          }
         }
-
-        if (min_heap.size() < top) {
-            min_heap.push(-val);
-        } else if (val < -min_heap.top()) {
-            min_heap.pop();
-            min_heap.push(-val);
-        }
+      }
     }
+    int64_t total = K * qsize;
+    return double(correct) / double(total);
+  }
+};
 
-    return std::make_pair(max_heap.top(), -min_heap.top());
-}
-
-float find_absmax(const float* data, int num_items, float ratio = 0.0f) {
-    int top = static_cast<int>(num_items * ratio) + 1;
-
-    std::priority_queue<float, std::vector<float>, std::greater<float>> heap;
-
-    for (int i = 0; i < num_items; ++i) {
-        float x = std::abs(data[i]);
-        if (heap.size() < top) {
-            heap.push(x);
-        } else if (x > heap.top()) {
-            heap.pop();
-            heap.push(x);
-        }
-    }
-
-    return heap.top();
-}
-
-float find_absmax_without_drop(const float* data, int num_items) {
-    float result = 0.0f;
-    for (int i = 0; i < num_items; ++i) {
-        result = std::max(result, std::abs(data[i]));
-    }
-    return result;
-}
